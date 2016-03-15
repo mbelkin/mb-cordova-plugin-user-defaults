8cddd1b0afc13c0fcc35b90ed4e1fee53b2cb48a
mbelkin <michael@belkininteractive.com>
2016-03-14 21:51:59 -0700

    first pass of actual functionality

--- src/ios/MBUserDefaults.m
+++ src/ios/MBUserDefaults.m
@@ -9,4 +9,86 @@
 
 @implementation MBUserDefaults
 
+- (void)saveValue:(CDVInvokedUrlCommand*)command
+{
+	CDVPluginResult * pluginResult = nil;
+	
+	NSString * key = command.arguments[0];
+	NSString * value = command.arguments[1];
+	
+	NSUserDefaults * userDefaults = [NSUserDefaults standardUserDefaults];
+	[userDefaults setObject:value forKey:key];
+	[userDefaults synchronize];
+	
+	if ([[userDefaults stringForKey:key] isEqualToString:value])
+	{
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+	}
+	else
+	{
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION];
+	}
+	
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)saveValueToAppGroup:(CDVInvokedUrlCommand*)command
+{
+	CDVPluginResult * pluginResult = nil;
+	
+	NSString * key = command.arguments[0];
+	NSString * value = command.arguments[1];
+	NSString * appGroupId = command.arguments[2];
+	
+	if (appGroupId.length == 0)
+	{
+		appGroupId = [NSString stringWithFormat:@"group.%@", [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"]];
+	}
+	
+	NSUserDefaults * userDefaults = [[NSUserDefaults alloc] initWithSuiteName:appGroupId];
+	[userDefaults setObject:value forKey:key];
+	[userDefaults synchronize];
+	
+	if ([[userDefaults stringForKey:key] isEqualToString:value])
+	{
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+	}
+	else
+	{
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION];
+	}
+	
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getValue:(CDVInvokedUrlCommand*)command
+{
+	NSString * key = command.arguments[0];
+	
+	NSUserDefaults * userDefaults = [NSUserDefaults standardUserDefaults];
+	NSString * value = [userDefaults stringForKey:key];
+	
+	CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:value];
+	
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getValueFromAppGroup:(CDVInvokedUrlCommand*)command
+{
+	NSString * key = command.arguments[0];
+	NSString * appGroupId = command.arguments[1];
+	
+	if (appGroupId.length == 0)
+	{
+		appGroupId = [NSString stringWithFormat:@"group.%@", [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"]];
+	}
+	
+	NSUserDefaults * userDefaults = [[NSUserDefaults alloc] initWithSuiteName:appGroupId];
+	NSString * value = [userDefaults stringForKey:key];
+	
+	CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:value];
+	
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
 @end
--- www/MBUserDefaults.js
+++ www/MBUserDefaults.js
@@ -1,7 +1,16 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    saveValue: function (key, value, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "MBUserDefaults", "saveValue", [key, value]);
+    },
+    saveValueToAppGroup: function (key, value, appGroup, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "MBUserDefaults", "saveValueToAppGroup", [key, value, appGroup]);
+    },
+    getValue: function (key, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "MBUserDefaults", "getValue", [key]);
+    },
+    getValueFromAppGroup: function (key, appGroup, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "MBUserDefaults", "getValueFromAppGroup", [key, appGroup]);
     }
 };
