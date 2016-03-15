3976da1a9f21dd9de5e87d3b5f4c47b44713123b
mbelkin <michael@belkininteractive.com>
2016-03-14 21:37:35 -0700

    renamed files

---
+++ src/ios/MBUserDefaults.h
@@ -0,0 +1,12 @@
+//
+//  MBUserDefaults.h
+//
+//  Created by Michael Belkin on 3/14/16.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface MBUserDefaults : CDVPlugin
+
+@end
---
+++ src/ios/MBUserDefaults.m
@@ -0,0 +1,12 @@
+//
+//  MBUserDefaults.m
+//
+//  Created by Michael Belkin on 3/14/16.
+//
+//
+
+#import "MBUserDefaults.h"
+
+@implementation MBUserDefaults
+
+@end
